7dd346cdbc22770ca0ed86dbd61b25bc5e006a9d
words <60527853+7000WORDS@users.noreply.github.com>
2022-02-24 15:28:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #Meant to open a text file which 
 #will then download a file from the internet and will be only accessible to those who have a special key to access it. 
+# we start by importing selenium so that we know when we finish downloading the files. 
+
+from selenium import webdriver
+from selenium.webdrive.common.by import By 
+from selenium.webdriver.common.keys import Keys
+
+
+browser = webdriver.chrome()
+browser.get("https://nike.com")
+assert "nike" in browser.title
+
+elem = browser.find_element(By.Name, 'p')
+elem.send_keys('nikehq'+ keys.RETURN)
